c71108ee3ba625cb676c59ccbe21d154696a1705
nayab9 <nayab9@gmail.com>
2014-05-06 16:38:59 +0000

    code cleanup part 1

--- cryptkeeper.c
+++ cryptkeeper.c
@@ -1,8 +1,31 @@
+/*****************************************************************
+ * 
+ * The Cryptkeeper
+ * 
+ * This utility is a simple demonstration of XOR encryption.
+ * Given a specified input of threads and a key file, the utility
+ * will proceed to XOR encrypt stdin data.
+ * 
+ * More information and detailed process can be found on github:
+ * https://github.com/nayab9/cryptkeeper
+ * 
+ * "Encrypto Patronus!" - A once famous Wizard
+ * 
+ * ****************************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
 
+/**************************************************
+ * 
+ * This struct stores thread information
+ * as the data is processed.
+ * Extra information is stored here for learning and
+ * debugging purposes.
+ * 
+ * ***************************************************/
 typedef struct block_info
 {
     int thread_id;
@@ -11,82 +34,119 @@ typedef struct block_info
     int encrypted_value;
 }block;
 
+/**************************************************
+ * 
+ * Thread function *encrypt():
+ * 
+ * Given a struct block, this function does the 
+ * thread processing.
+ * 
+ * Each block contains the data value and encryption
+ * value, it will XOR them together and return the block
+ * 
+ * EXCEPTION CASE:
+ * In the case where the encryption and data value are
+ * identical, we hit a gotcha - they cancel eachother
+ * out and equal 0. Thus I leave this value unecrypted
+ * for the purpose of this demonstration.
+ * 
+ * ***************************************************/
 void *encrypt(void *thread_ptr)
 {
     block *data = (block *) thread_ptr;
-    
-    //printf("This is my passed thread id: %x\n", data->thread_id);
-    //printf("This is my passed data value: %x\n", data->data_value);
-    //printf("This is my passed key value: %x\n", data->key_value);
+
     if ( data->key_value != data->data_value )
     {
         data->encrypted_value = data->key_value ^ data->data_value;
     }
     else
     {
-        //printf("Does it hit this ******************\n");
         data->encrypted_value = data->data_value;
     }
-    //printf("This is the new encrypted value: %x\n", data->encrypted_value);
-    
+
     return (void * ) data;
 }
 
+/**************************************************
+ * 
+ * main():
+ * 
+ * The main processing is done as follows:
+ * 1. Read command line inputs -> assign threads and key
+ * 2. Read the key file (static file, does not change)
+ *   - key file indicates blocksize
+ * 3. Read data input from stdin in bytes (blocksize)
+ *   - spawn threads as required
+ *   - join threads as required
+ * 4. Output data to stdout
+ * 
+ * Detailed explanations can be found here:
+ * https://github.com/nayab9/cryptkeeper
+ * 
+ * ***************************************************/
 int main(int argc, char *argv[])
 {
-    //put a switch statement here for input paramaters, assign variables as needed.
-    int num_threads = 0;
     char *keyfile;
-    
+    char *source = NULL;
+    int num_threads = 0;
+    int length = argc - 1;
+    int loop, i, j;
+        
+    //valid input parameters
     char *key_switch = "-k";
     char *thread_switch = "-n";
-    
-    int length = argc - 1;
-    int loop;
-    
-    //printf("Length is: %d\n", length);
-    
+
+    //find input parameters and capture them   
     for (loop = 0; loop < length; loop++)
     {
-        //printf("loop is: %d\n", loop);
-        
         if (strcmp (argv[loop], key_switch) == 0)
         {
-            //printf("Found a match for keyfile switch: %s\n", argv[loop+1]);
             keyfile = argv[loop+1];
         }
         
         if (strcmp (argv[loop], thread_switch) == 0)
         {
-            //printf("Found a match for threads switch: %s\n", argv[loop+1]);
             sscanf (argv[loop+1], "%d", &num_threads);
         }
-
+        
+        /*
+        if (strcmp (argv[loop], thread_switch) == 0)
+        {
+            //add more switch options here...
+        }
+        */
     }
     
     //read from the key file specified
- 
-    char *source = NULL;
     FILE *fp = fopen(keyfile, "r");
-    if (fp != NULL) {
-        /* Go to the end of the file. */
-        if (fseek(fp, 0L, SEEK_END) == 0) {
-            /* Get the size of the file. */
+    if (fp != NULL) 
+    {
+        if (fseek(fp, 0L, SEEK_END) == 0) 
+        {
             long bufsize = ftell(fp);
-            if (bufsize == -1) { /* Error */ }
+            if (bufsize == -1) 
+            {
+                fprintf(stderr, "Error reading keyfile, exiting.. \n");
+                return 2;                    
+            }
     
-            /* Allocate our buffer to that size. */
             source = (char *)malloc(sizeof(char) * (bufsize + 1));
     
-            /* Go back to the start of the file. */
-            if (fseek(fp, 0L, SEEK_SET) != 0) { /* Error */ }
+            if (fseek(fp, 0L, SEEK_SET) != 0) 
+            {
+                fprintf(stderr, "Error reading keyfile, exiting.. \n");
+                return 2;  
+            }
     
-            /* Read the entire file into memory. */
             size_t newLen = fread(source, sizeof(char), bufsize, fp);
-            if (newLen == 0) {
-                fputs("Error reading file", stderr);
-            } else {
-                source[++newLen] = '\0'; /* Just to be safe. */
+            if (newLen == 0) 
+            {
+                fprintf(stderr, "Error opening keyfile, exiting.. \n");
+                return 2;    
+            } 
+            else 
+            {
+                source[++newLen] = '\0';
             }
         }
         fclose(fp);
@@ -97,132 +157,111 @@ int main(int argc, char *argv[])
         return 2;       
     }
 
+    //assign a pointer to the keyfile
+    //this will be used from here on out for encrypting
     char *buff = source;
 
-    //printf("You have chosen to utilize %d number of threads for this process.\n", num_threads);
-    //printf("The key file chosen was %s.\n", buff);
-    //printf("");
-
+    //retrive the blocksize based on the keyfile
     int blocksize = strlen(buff);
-    //printf("blocksize is: %d\n", blocksize);
-    char text[blocksize];
     
+    //if threads specified is more than the blocksize
+    //force threads to blocksize, since thats the max
+    //number of threads we can utilize
     if (num_threads > blocksize)
     {
         num_threads = blocksize;
     }
     
-    char* plaintext = text;
-    char *buff2 = plaintext;
-    int i, j;
-    
-    //read bytes, since we can't rely on characters themselves (i.e Unicode vs ASCII)
+    //this is where we store the XOR'd values to printout
+    char text[blocksize];
+
+    //read by bytes, since we can't rely on characters themselves (i.e Unicode vs ASCII)
     int result = fread(text, sizeof(char), blocksize, stdin);
     
-    //printf("The initial read was: %d\n", result);
-    
+    //initialize the struct blocks and threads needed for processing
     block encrypt_go[blocksize];
     pthread_t threads[num_threads];
     
     //so long as enough blocksize bytes are available, continue looping
 	//no threading will be used if they don't want it (i.e. threads > 2)
     while ( num_threads >= 2 && result == blocksize )
-	//while ( num_threads >= 2 && blocksize > result )
     {
-        //printf("The plaintext read in is: %s\n", plaintext);
-        //printf("the result of the read was: %d\n", result);
-
-            //XOR the block read in with our keyfile provided, print to stdout
-            //spawn thread for this work
-
+        //loop through block and encrypt
+        for(i=0; i <= blocksize; i++)
+        {
+            int index = i % num_threads;
+            int temp_i = i;
             
-            for(i=0; i <= blocksize; i++)
-	    //for(i=0; i <= result; i++)
+    		if (temp_i < result)
+    		{
+            	encrypt_go[temp_i].thread_id = temp_i;
+            	encrypt_go[temp_i].data_value = text[temp_i];
+            	encrypt_go[temp_i].key_value = buff[temp_i];
+    		}
+
+            //whats needed here is the wrap up processing if i > num_threads
+            if ((temp_i >= num_threads && index == 0) ||
+                 blocksize == num_threads && num_threads == temp_i )
+                 //|| temp_i == result && result )
             {
-                //printf("What if I just print what text[i] actually is here before processing?: %x\n", text[i]);
-                //text[i] ^= buff[i];
-                //printf("The XOR result of block %d is %x\n", i, text[i]);
-                //printf("%c\n", text[i]);
-                int index = i % num_threads;
-                int temp_i = i;
-                
-        		if (temp_i < result)
-        		{
-                        	encrypt_go[temp_i].thread_id = temp_i;
-                        	encrypt_go[temp_i].data_value = text[temp_i];
-                        	encrypt_go[temp_i].key_value = buff[temp_i];
-        		}
- 
-                //printf("the thread value i am now going to pass is: %x\n", encrypt_go[index].thread_id);
-                //printf("the data value i am now going to pass is: %x\n", encrypt_go[index].data_value);
-               // printf("the key value i am now going to pass is: %x\n", encrypt_go[index].key_value);
-                
-                //@@@@@@@@@@@@
-                //index is going to overlap
-                
-                //whats needed here is the wrap up processing if i > num_threads
-                if ((temp_i >= num_threads && index == 0) ||
-                     blocksize == num_threads && num_threads == temp_i )
-                     //|| temp_i == result && result )
+                //printf("I should wrap it up right MEOW!\n") ;   
+                //this will seg fault if num_threads is less than blocksize
+                //wait for threads to finish processing
+                for (j = 0; j < num_threads; j++)
                 {
-                    //printf("I should wrap it up right MEOW!\n") ;   
-                    //this will seg fault if num_threads is less than blocksize
-                    //wait for threads to finish processing
-                    for (j = 0; j < num_threads; j++)
+                    void *returnValue;
+ 		//printf("I did qualify for wrapping up, my i value is %d, and index is %d\n", temp_i, index);                       
+                    if (pthread_join( threads[j], &returnValue ))
                     {
-                        void *returnValue;
- 			//printf("I did qualify for wrapping up, my i value is %d, and index is %d\n", temp_i, index);                       
-                        if (pthread_join( threads[j], &returnValue ))
-                        {
-                            fprintf(stderr, "error joining thread \n");
-                            return 2;
-                        }
-                        else
-                        {
-                            block *return_block = (block *) returnValue;
-                            int encrypted_value = (int) return_block->encrypted_value;
-                            int returned_thread = (int) return_block->thread_id;
-                            
-                            //printf("This is the thread id return value of the thread       #%d: %x\n", j, (int) return_block->thread_id);
-                            //printf("This is the data value return value of the thread      #%d: %x\n", j, (int) return_block->data_value);
-                            //printf("This is the key value return value of the thread       #%d: %x\n", j, (int) return_block->key_value);
-                            //printf("This is the encrypted value return value of the thread #%d: %x\n", j, (int) return_block->encrypted_value);
-                            text[returned_thread] = encrypted_value;
-                            //printf("%x", text[returned_thread]);
-                        }
-                    }
-                   
-        			if (temp_i < result)
-        			{ 
-        				//printf("I'm still not done the block, I'm creating a new thread overlapping at index: %d, with i value: %d", index, temp_i);
-        		           if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
-        		            {
-        		                fprintf(stderr, "Error creating thread \n");
-        		                return 1;
-        		            }
-        		            else
-        		            {
-        		                //printf("I sent this through to the thread #%d: %x\n", index, encrypt_go[index].data_value);
-        		            } 
-        			}                                   
-                    
-                }
-                else
-                {
-                    //printf("I didn't qualify for wrapping up, my i value is %d, and index is %d\n", temp_i, index);
-                    if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
-                    {
-                        fprintf(stderr, "Error creating thread \n");
-                        return 1;
+                        fprintf(stderr, "error joining thread \n");
+                        return 2;
                     }
                     else
                     {
-                        //printf("I sent this through to the thread #%d: %x\n", index, encrypt_go[index].data_value);
-                    }                
-                    
+                        block *return_block = (block *) returnValue;
+                        int encrypted_value = (int) return_block->encrypted_value;
+                        int returned_thread = (int) return_block->thread_id;
+                        
+                        //printf("This is the thread id return value of the thread       #%d: %x\n", j, (int) return_block->thread_id);
+                        //printf("This is the data value return value of the thread      #%d: %x\n", j, (int) return_block->data_value);
+                        //printf("This is the key value return value of the thread       #%d: %x\n", j, (int) return_block->key_value);
+                        //printf("This is the encrypted value return value of the thread #%d: %x\n", j, (int) return_block->encrypted_value);
+                        text[returned_thread] = encrypted_value;
+                        //printf("%x", text[returned_thread]);
+                    }
                 }
-
+               
+    			if (temp_i < result)
+    			{ 
+    				//printf("I'm still not done the block, I'm creating a new thread overlapping at index: %d, with i value: %d", index, temp_i);
+    		           if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
+    		            {
+    		                fprintf(stderr, "Error creating thread \n");
+    		                return 1;
+    		            }
+    		            else
+    		            {
+    		                //printf("I sent this through to the thread #%d: %x\n", index, encrypt_go[index].data_value);
+    		            } 
+    			}                                   
+                
             }
+            else
+            {
+                //printf("I didn't qualify for wrapping up, my i value is %d, and index is %d\n", temp_i, index);
+                if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
+                {
+                    fprintf(stderr, "Error creating thread \n");
+                    return 1;
+                }
+                else
+                {
+                    //printf("I sent this through to the thread #%d: %x\n", index, encrypt_go[index].data_value);
+                }                
+                
+            }
+
+        }
             
        //printf ("the entire string is what? %s\n", text);
     	printf("%s", text);
