59406867a2b1ce5ccb8981939fa7990fa9b819d1
nayab9 <nayab9@gmail.com>
2014-05-06 22:10:08 +0000

    code cleanup part 2

--- cryptkeeper.c
+++ cryptkeeper.c
@@ -9,7 +9,7 @@
  * More information and detailed process can be found on github:
  * https://github.com/nayab9/cryptkeeper
  * 
- * "Encrypto Patronus!" - A once famous Wizard
+ * "Encrypto Patronum!" - A once famous Wizard
  * 
  * ****************************************************************/
 
@@ -186,12 +186,20 @@ int main(int argc, char *argv[])
 	//no threading will be used if they don't want it (i.e. threads > 2)
     while ( num_threads >= 2 && result == blocksize )
     {
-        //loop through block and encrypt
+        //loop through the block (of bytes) that was read in
         for(i=0; i <= blocksize; i++)
         {
+            //index is the modulus of i, this allows us to keep track of
+            //which thread we are processing when the blocksize is greater than
+            //the number of threads that are requsted
+            //example: 2 threads requsted, but our blocksize is 4, thus
+            //we are limited to using 2 threads, twice (thread 1 and 2, twice)
             int index = i % num_threads;
+            
+            //local scope reference to i, to be multi-thread safe
             int temp_i = i;
             
+            //safety check, only fill data the size of the blocks
     		if (temp_i < result)
     		{
             	encrypt_go[temp_i].thread_id = temp_i;
@@ -199,21 +207,18 @@ int main(int argc, char *argv[])
             	encrypt_go[temp_i].key_value = buff[temp_i];
     		}
 
-            //whats needed here is the wrap up processing if i > num_threads
+            //if we have less threads than the blocksize, we wrap up the thread
+            //processing before processing the rest of the block
             if ((temp_i >= num_threads && index == 0) ||
                  blocksize == num_threads && num_threads == temp_i )
-                 //|| temp_i == result && result )
             {
-                //printf("I should wrap it up right MEOW!\n") ;   
-                //this will seg fault if num_threads is less than blocksize
-                //wait for threads to finish processing
+                //for each thread spawned, we will attempt to join them together
                 for (j = 0; j < num_threads; j++)
                 {
                     void *returnValue;
- 		//printf("I did qualify for wrapping up, my i value is %d, and index is %d\n", temp_i, index);                       
                     if (pthread_join( threads[j], &returnValue ))
                     {
-                        fprintf(stderr, "error joining thread \n");
+                        fprintf(stderr, "Error joining thread, exiting.. \n");
                         return 2;
                     }
                     else
@@ -221,79 +226,70 @@ int main(int argc, char *argv[])
                         block *return_block = (block *) returnValue;
                         int encrypted_value = (int) return_block->encrypted_value;
                         int returned_thread = (int) return_block->thread_id;
-                        
-                        //printf("This is the thread id return value of the thread       #%d: %x\n", j, (int) return_block->thread_id);
-                        //printf("This is the data value return value of the thread      #%d: %x\n", j, (int) return_block->data_value);
-                        //printf("This is the key value return value of the thread       #%d: %x\n", j, (int) return_block->key_value);
-                        //printf("This is the encrypted value return value of the thread #%d: %x\n", j, (int) return_block->encrypted_value);
+
                         text[returned_thread] = encrypted_value;
-                        //printf("%x", text[returned_thread]);
                     }
                 }
-               
-    			if (temp_i < result)
-    			{ 
-    				//printf("I'm still not done the block, I'm creating a new thread overlapping at index: %d, with i value: %d", index, temp_i);
-    		           if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
-    		            {
-    		                fprintf(stderr, "Error creating thread \n");
-    		                return 1;
-    		            }
-    		            else
-    		            {
-    		                //printf("I sent this through to the thread #%d: %x\n", index, encrypt_go[index].data_value);
-    		            } 
-    			}                                   
+                //if our block size is larger than number of threads
+                //we must now processing the rest of the block
+            	if (temp_i < result)
+            	{ 
+                       if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
+                        {
+                            fprintf(stderr, "Error creating thread, exiting... \n");
+                            return 1;
+                        }
+            	}                                   
                 
             }
+            //we still have threads which can process some of our blockdata, continue.
             else
             {
-                //printf("I didn't qualify for wrapping up, my i value is %d, and index is %d\n", temp_i, index);
                 if (pthread_create( &threads[index], NULL, encrypt, (void *) &encrypt_go[temp_i] ))
                 {
                     fprintf(stderr, "Error creating thread \n");
                     return 1;
                 }
-                else
-                {
-                    //printf("I sent this through to the thread #%d: %x\n", index, encrypt_go[index].data_value);
-                }                
-                
             }
 
         }
             
-       //printf ("the entire string is what? %s\n", text);
+        //block processing complete, print to stdout
     	printf("%s", text);
-
+    	
+    	//read the next available block (bytes of blocksize)
         result = fread(text, sizeof(char), blocksize, stdin);
-        //printf("The next readin is %d\n", result);
+        
+        /*******************************************
+         * Circular shift:
+         * 
+         * Shift the bits left by one, while retaining
+         * the lost bit and placing it as the LSB
+         * *****************************************/
+        /*
+    	//shift the bits in the keyfile to the left by one after each block is processed
+    	int loop;
+
+    	for (loop = 0; loop < strlen(buff); loop++)
+    	{
+	        //y = (x << shift) | ( (x >> (sizeof(x)* CHAR_BIT - shift)) 
+    		buff[loop] = (buff[loop] << 1) | (buff[loop] >> (sizeof(buff[loop]) * 8 - 1));
+    	}
+
+        */
     }
 
-    //we've reached the end of the plaintext file
-    //if there are any leftover data pieces (which will be smaller than block size)
-    //finish them off so we can complete the process.
-
-    //so long as enough blocksize bytes are available, continue looping
+    //in the event that there is leftover data at the end of a finite size file
+    //we will processing the remaining blocks individually without threads, to save overhead
     while ( result > 0 )
     {
-        ////printf("The leftover plaintext read in is: %s\n", text);
-        ////printf("the result of the read was: %d\n", result);
-
-            //XOR the block read in with our keyfile provided, print to stdout
-            for(i=0; i < result; i++)
-            {
-                text[i] ^= buff[i];
-                printf ("%c", text[i]);
-                //printf("The XOR result of block %d is %x\n", i, text[i]);
-                //printf("%c\n", text[i]);
-            }
-    
+        //XOR the block read in with our keyfile provided, print to stdout
+        for(i=0; i < result; i++)
+        {
+            text[i] ^= buff[i];
+            printf ("%c", text[i]);
+        }
+        
         result = fread(text, sizeof(char), blocksize, stdin);
-        //printf("The next readin is %d\n", result);
     }
- 
-
-    free(source); /* Don't forget to call free() later! */
-
 }
